ebe7e11f6887d5eae80fe7ee1ac847601c02df47
Varsha-eluri <varshaeluri123@gmail.com>
2025-06-30 15:17:59 +0530

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,8 @@
+age=18
+if age>18:
+    print("Eligible")
+elif age==18:
+    print("new voter")
+else:
+    print("not eligible")
+print("if condition executed")
---
+++ conditional.py
@@ -0,0 +1,5 @@
+a=int(input("enter a to check"))
+if a%10==7 or a/10==7:
+    print("lucky no")
+else :
+    print("not lucky")
